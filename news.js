73c3d53528ba453c8df55aee4c72dc1dd8fce1de
rony9669 <ronychowdhury.cse@gmail.com>
2022-09-04 03:39:42 +0600

    Responsive data added

--- news.js
+++ news.js
@@ -1,19 +1,20 @@
 const allCategories = async() => {
-    const response = await fetch(
-        "https://openapi.programming-hero.com/api/news/categories"
-    );
-    const data = await response.json();
-    const finalData = data.data.news_category;
-    return finalData;
-    // return data;
+    try {
+        const response = await fetch(
+            "https://openapi.programming-hero.com/api/news/categories"
+        );
+        const data = await response.json();
+        const finalData = data.data.news_category;
+        return finalData;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 const displayCategory = allCategories().then((data) => {
-    // console.log(data);
     const categoryField = document.getElementById("all-category");
 
     data.map((element) => {
-        // console.log(data);
         const { category_name, category_id } = element;
 
         const div = document.createElement("div");
@@ -38,7 +39,9 @@ const allCategoriesDetails = (category_id) => {
 
 const displayCategoryDetails = (data) => {
     const foundedSize = document.getElementById("founded-size");
-    foundedSize.innerHTML = `<p class="text-center fw-bold"> ${data.length} items found for the category</p>`;
+    foundedSize.innerHTML = `<p class="text-center fw-bold"> ${
+    data.length !== 0 ? data.length : "No "
+  } news found for this category</p>`;
 
     const categoryDetailsField = document.getElementById("category-info");
     categoryDetailsField.textContent = "";
@@ -52,14 +55,12 @@ const displayCategoryDetails = (data) => {
         const {
             title,
             total_view,
+            image_url,
             thumbnail_url,
             details,
             author,
-            category_id,
             _id,
         } = elements;
-
-        // console.log(_id);
 
         const { name, img } = author;
         const showDetailsLimit = details.slice(0, 200).concat("...");
@@ -67,12 +68,12 @@ const displayCategoryDetails = (data) => {
         div.innerHTML = `
         <div class="card mb-3">
                 <div class="row ">
-                    <div class="col-md-3">
+                    <div class="col-lg-4  col-md-6 col-sm-12">
                             <img src="${
-                              thumbnail_url ? thumbnail_url : "No data Found"
-                            } : No image Found" class="img-fluid rounded-start" alt="..."  />
+                              image_url ? image_url : "No Data"
+                            } : No image Found" class="img-fluid" alt="..."  />
                     </div>
-                    <div class="col-md-9">
+                    <div class="col-lg-8 col-md-6 col-sm-12">
                         <div class="card-body">
                             <h5 class="card-title">${title}</h5>
                             <p class="card-text">
@@ -81,25 +82,23 @@ const displayCategoryDetails = (data) => {
                             <div class="d-flex justify-content-between mt-5" ">
                                 <div>
                                     <img src="${
-                                      img ? img : "No data Found"
+                                      img ? img : "No Data"
                                     }" width="45" alt="..." class="rounded img-fluid d-inline-block align-middle" alt="..." />
 
                                     <span class="fw-bold ">${
-                                      name ? name : "No Name Found"
+                                      name ? name : "No Name"
                                     }</span>
                                 </div>
                                 <div>
                                     <img src="./image/view.jpg " width="45 " alt="... ">
                                     <span>${
-                                      total_view ? total_view : "No data Found"
+                                      total_view ? total_view : "No Data"
                                     }</span>
                                 </div>
                                 <div>
                                 <button href="#" onClick="allCategoriesFullDetails(('${_id}'))" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">Show Details</button>
-                                </div>
-                                
+                                </div> 
                         </div>
-
                 </div>
             </div>
         `;
@@ -132,14 +131,12 @@ const allCategoriesFullDetails = (news_id) => {
 };
 
 const displayCategoryFullDetails = (data) => {
-    // console.log(data[0]._id);
-
     const newsDetails = document.getElementById("news-details");
 
     data.forEach((element) => {
         const { total_view, title, image_url, details, author } = element;
         const { name, img, published_date } = author;
-        // console.log(title);
+
         const newsTitle = document.getElementById("newsDetailsModalLabel");
 
         newsTitle.innerHTML = `<div class="fw-bold">Title: ${
@@ -170,3 +167,5 @@ const displayCategoryFullDetails = (data) => {
 // allCategories(08);
 
 // displayCategory(08);
+
+allCategoriesDetails("08");
